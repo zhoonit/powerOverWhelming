cbe85a91fcf5e1c9baa8465d27bc43e144083ee0
lunapocket <lunapocket@naver.com>
2017-05-29 14:33:58 +0900

    file structure refactored

--- crawling/execution_C++.py
+++
@@ -1,5 +0,0 @@
-from subprocess import *
-p = subprocess.Popen([r"/usr/bin/g++", "-Wall", "-o", "test", 'test.cpp'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-p.communicate()
-p = subprocess.Popen(["./test"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-p.communicate()
